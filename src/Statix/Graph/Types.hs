47f07be1b7fb2841cfca43b2ff55a491b5ceef93
Arjen Rouvoet <a.j.rouvoet@gmail.com>
2019-03-12 13:54:50 +0100

    [ ✓ ] Interpreted generic findReachable for graphs

--- src/Statix/Graph/Types.hs
+++ src/Statix/Graph/Types.hs
@@ -16,9 +16,30 @@ class (Monad m, Eq l, Ord n) => MonadGraph n l d m | m -> n l d where
   getDatum :: n → m (Maybe d)
   getOutEdges :: n → m [(l, n)]
 
-  runQuery :: n → Regex l → m [Path n l]
-  runQuery n re = _resolve n re Set.empty
-    where
+-- | Find reachable nodes in the graph over a regex.
+-- Together with the regex derivative in that node
+findReachable :: (MonadGraph n l d m) ⇒ n → Regex l → m [(n, Regex l)]
+findReachable n re = _find n re Set.empty
+  where
+    _find :: (MonadGraph n l d m) ⇒ n → Regex l → Set n → m [(n, Regex l)]
+    _find n re vis
+      | Re.empty re = return []
+      | otherwise   = do
+       -- check if we visited this node yet on the path here
+       if Set.member n vis
+         then return []
+         else do
+           -- get the edges out of here
+           es ← getOutEdges n
+           -- calculate the paths out of here
+           reachables ← concat <$> mapM (\(l, m) → _find m (Re.match l re) vis) es 
+           -- add the loop if it exists
+           return ((n, re) : reachables)
+
+-- | Find regular paths in the graph
+runQuery :: (MonadGraph n l d m) ⇒ n → Regex l → m [Path n l]
+runQuery n re = _resolve n re Set.empty
+  where
     _resolve n re vis
       | Re.empty re = return []
       | otherwise   = do
