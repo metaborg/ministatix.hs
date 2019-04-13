8b4b54d1dffa9efa48fdda16d4ffef009202a455
Arjen Rouvoet <a.j.rouvoet@gmail.com>
2019-04-13 17:40:43 +0200

    [ add ] Some font-faces to ministatix.el

--- emacs/ministatix.el
+++ emacs/ministatix.el
@@ -1,8 +1,8 @@
-;;; statix.el
+;;; ministatix.el
 
-(defvar statix-mode-syntax-table nil "Syntax table for statix-mode.")
+(defvar ministatix-mode-syntax-table nil "Syntax table for statix-mode.")
 
-(setq statix-mode-syntax-table
+(setq ministatix-mode-syntax-table
       (let ( (synTable (make-syntax-table)))
         (modify-syntax-entry ?\/ ". 12b" synTable)
         (modify-syntax-entry ?\n "> b" synTable)
@@ -10,7 +10,7 @@
 
 ;; create the list for font-lock.
 ;; each category of keyword is given a particular face
-(setq statix-font-lock-keywords
+(setq ministatix-font-lock-keywords
       (let* (
              ;; define several category of keywords
              (x-keywords '("import" "match" "true" "false" "in" "as" "query" "new"))
@@ -25,15 +25,17 @@
           (,x-builtins-regexp . font-lock-constant-face)
           (,x-keywords-regexp . font-lock-keyword-face)
 	  (,"=\\|->\\|\\-\\[\\|\\]->\\|:-\\||" . font-lock-keyword-face)
-	  (,"\\(\\w+\\)(" (1 font-lock-variable-name-face))
+	  (,"\\(\\w+\\)(" (1 font-lock-function-name-face))
           )))
 
-(define-derived-mode statix-mode prog-mode "ministatix mode"
+(define-derived-mode ministatix-mode prog-mode "ministatix mode"
   "Major mode for editing Ministatix constraint language"
 
   ;; code for syntax highlighting
-  (setq font-lock-defaults '((statix-font-lock-keywords))))
+  (setq font-lock-defaults '((ministatix-font-lock-keywords))))
 
+
+(add-to-list 'auto-mode-alist '("\\.stx\\'" . ministatix-mode))
 
 ;; add the mode to the `features' list
-(provide 'statix-mode)
+(provide 'ministatix-mode)
