0288cf06bb97ea2e3d2c6f5668b4686059fca835
Arjen Rouvoet <a.j.rouvoet@gmail.com>
2019-04-18 14:40:03 +0200

    [ fix ] Be a little more liberal in query computation

--- src/Statix/Graph/Paths.hs
+++ src/Statix/Graph/Paths.hs
@@ -1,6 +1,9 @@
 module Statix.Graph.Paths where
 
 import Data.Relation
+import Data.Map as Map hiding (toList, foldl)
+import Data.Set as Set hiding (toList, foldl)
+
 
 import Control.Monad.Reader
 import Control.Monad.Trans
@@ -41,3 +44,44 @@ setLeMin le (x:xs) = snd $ foldl f (x , [x]) xs
         then (rep, e:acc)
         else (e, [e])     -- RIP king, long live the king
       else (rep, acc)
+
+-- | Find reachable nodes in the graph over a regex.
+-- Together with the regex derivative in that node
+findReachable :: (Ord l, MonadGraph n l d m) ⇒ n → Regex l → m (Map n (Regex l))
+findReachable n re = _find n re Set.empty
+  where
+    _find n re vis
+      | Re.empty re = return Map.empty
+      | otherwise   = do
+       -- check if we visited this node yet on the path here
+       if Set.member n vis
+         then return Map.empty
+         else do
+           -- put n in visisted set
+           let vis' = Set.insert n vis
+           -- get the edges out of here
+           es ← getOutEdges n
+           reachables ← Map.unionsWith RAlt <$> mapM (\(l, m) → _find m (Re.match l re) vis') es
+           return (Map.insertWith RAlt n re reachables)
+
+-- | Find regular paths in the graph
+runQuery :: (MonadGraph n l d m) ⇒ n → Regex l → m [Path n l]
+runQuery n re = _resolve n re Set.empty
+  where
+    _resolve n re vis
+      | Re.empty re = return $ (if Re.matchε re then [ End n ] else [])
+      | otherwise   = do
+       -- check if we visited this node yet on the path here
+       if Set.member n vis
+         then return []
+         else do
+           -- get the edges out of here
+           es ← getOutEdges n
+           -- calculate the paths out of here
+           pss ← mapM (\ (l , tgt) → do
+                     ps ← _resolve tgt (Re.match l re) (Set.insert n vis)
+                     return (fmap (Via (n , l)) ps)
+                   ) es
+           let ps = concat pss
+           -- add the loop if it exists
+           return (if Re.matchε re then (End n : ps) else ps)
--- src/Statix/Graph/Types.hs
+++ src/Statix/Graph/Types.hs
@@ -1,9 +1,5 @@
 module Statix.Graph.Types where
 
-import Control.Monad.Reader
-import Data.Set as Set
-import Data.Map as Map
-
 import Statix.Regex as Re
 
 data Path n l = End n | Via (n , l) (Path n l)
@@ -18,44 +14,3 @@ class (Monad m, Eq l, Ord n) => MonadGraph n l d m | m -> n l d where
   getDatum :: n → m (Maybe d)
   setDatum :: n → d → m ()
   getOutEdges :: n → m [(l, n)]
-
--- | Find reachable nodes in the graph over a regex.
--- Together with the regex derivative in that node
-findReachable :: (Ord l, MonadGraph n l d m) ⇒ n → Regex l → m (Map n (Regex l))
-findReachable n re = _find n re Set.empty
-  where
-    _find n re vis
-      | Re.empty re = return Map.empty
-      | otherwise   = do
-       -- check if we visited this node yet on the path here
-       if Set.member n vis
-         then return Map.empty
-         else do
-           -- put n in visisted set
-           let vis' = Set.insert n vis
-           -- get the edges out of here
-           es ← getOutEdges n
-           reachables ← Map.unionsWith RAlt <$> mapM (\(l, m) → _find m (Re.match l re) vis') es
-           return (Map.insertWith RAlt n re reachables)
-
--- | Find regular paths in the graph
-runQuery :: (MonadGraph n l d m) ⇒ n → Regex l → m [Path n l]
-runQuery n re = _resolve n re Set.empty
-  where
-    _resolve n re vis
-      | Re.empty re = return []
-      | otherwise   = do
-       -- check if we visited this node yet on the path here
-       if Set.member n vis
-         then return []
-         else do
-           -- get the edges out of here
-           es ← getOutEdges n
-           -- calculate the paths out of here
-           pss ← mapM (\ (l , tgt) → do
-                     ps ← _resolve tgt (Re.match l re) (Set.insert n vis)
-                     return (fmap (Via (n , l)) ps)
-                   ) es
-           let ps = concat pss
-           -- add the loop if it exists
-           return (if Re.matchε re then (End n : ps) else ps)
