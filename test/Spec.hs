e77d3b12e1f14bcce82733e0f2556a000f915b9c
Arjen Rouvoet <a.j.rouvoet@gmail.com>
2019-05-04 14:19:02 +0200

    [ fix ] Spec tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -49,10 +49,8 @@ run o c = do
   let mark = if o then "[✓]" else "[×]"
   describe (mark ++ " " ++ c) $ do
     -- parsing
-    let tokens = lexer c
-    let parsed = tokens >>= runParser specmod . parseConstraint
+    let parsed = parseConstraint specmod c
     it "parses" $ do
-      isRight tokens `shouldBe` True
       isRight parsed `shouldBe` True
 
     -- static analysis
@@ -68,29 +66,29 @@ corespec = do
   describe "equality" $ do
     run True  "{x} x = x"
     run True  "{x, y} x = y"
-    run True  "{x} f(x) = f(x)"
-    run True  "{x, y} f(x) = y"
-    run True  "{x, y} f(x) = f(g(y))"
+    run True  "{x} F(x) = F(x)"
+    run True  "{x, y} F(x) = y"
+    run True  "{x, y} F(x) = F(G(y))"
 
     describe "occurs check" $ do
-      run False "{x} f(x) = x"
-      run False "{x} f(x) = g(x)"
-      run False "{x, y} f(x) = g(y)"
-      run False "{x, y} f(x) = f(g(x))"
-      run False "{x, y} f(x) = g(f(x))"
+      run False "{x} F(x) = x"
+      run False "{x} F(x) = G(x)"
+      run False "{x, y} F(x) = G(y)"
+      run False "{x, y} F(x) = F(G(x))"
+      run False "{x, y} F(x) = G(F(x))"
 
     describe "n-ary" $ do
-      run False "{x, y} f(x) = f(x, y)"
-      run True  "{x, y} f(x, y) = f(y, x)"
-      run False "{x, y} f(x, y) = f(y, x), x = f(y)"
-      run False "{x, y} f(x, y) = g(x, y)"
+      run False "{x, y} F(x) = F(x, y)"
+      run True  "{x, y} F(x, y) = F(y, x)"
+      run False "{x, y} F(x, y) = F(y, x), x = F(y)"
+      run False "{x, y} F(x, y) = G(x, y)"
   
     describe "transitive" $ do
-      run True "{x, y} f() = x, x = y, y = f()"
-      run False "{x, y} f() = x, x = y, y = g()"
-      run False "{x, y} f(y) = x, x = y"
-      run False "{x, y, z} f(x) = z, x = y, y = z"
-      run True  "{x, y, z} f(x) = z, x = y, f(y) = z"
+      run True "{x, y} F() = x, x = y, y = F()"
+      run False "{x, y} F() = x, x = y, y = G()"
+      run False "{x, y} F(y) = x, x = y"
+      run False "{x, y, z} F(x) = z, x = y, y = z"
+      run True  "{x, y, z} F(x) = z, x = y, F(y) = z"
 
   describe "stuckness detection" $ do
     run False "{x, z} query x `P as z"
@@ -106,56 +104,58 @@ newspec = describe "new" $ do
 queryspec :: Spec
 queryspec = describe "query" $ do
   describe "only" $ do
-    run False "{x, y, z} new x, query x `l as y, only(y , z)"
-    run True  "{x, y, z} new x, query x `l* as y, only(y , z)"
-    run False "{x, y, z} new x, query x `l+ as y, only(y , z)"
-    run False "{x, y, z} new x, query x `l`p as y, only(y , z)"
-    run True  "{x,y,z,zt} new x, new y, x -[ `l ]-> y, query x `l+ as z, only(z, zt)"
-    run False "{x,y,yy,z,zt} new x, new y, new yy, x -[ `l ]-> y, y -[ `l ]-> yy, query x `l+ as z, only(z, zt)"
-    run False "{x,y,z,zt} new x, new y, x -[ `l ]-> y, y -[ `l ]-> x, query x `l+ as z, only(z, zt)"
-    run False "{x,y,z,zt} new x, new y, x -[ `l ]-> y, query x `l* as z, only(z, zt)"
-    run True  "{x,y,z,zt} new x, new y, query x `l+ as z, x -[ `l ]-> y, only(z, zt)"
-    run False "{x,y,z,zt} new x, new y, query x `l* as z, x -[ `l ]-> y, only(z, zt)"
+    run False "{x, y, z} new x, query x `L as y, only(y , z)"
+    run True  "{x, y, z} new x, query x `L* as y, only(y , z)"
+    run False "{x, y, z} new x, query x `L+ as y, only(y , z)"
+    run False "{x, y, z} new x, query x `L`P as y, only(y , z)"
+    run True  "{x,y,z,zt} new x, new y, x -[ `L ]-> y, query x `L+ as z, only(z, zt)"
+    run False "{x,y,yy,z,zt} new x, new y, new yy, x -[ `L ]-> y, y -[ `L ]-> yy, query x `L+ as z, only(z, zt)"
+    run False "{x,y,z,zt} new x, new y, x -[ `L ]-> y, y -[ `L ]-> x, query x `L+ as z, only(z, zt)"
+    run False "{x,y,z,zt} new x, new y, x -[ `L ]-> y, query x `L* as z, only(z, zt)"
+    run True  "{x,y,z,zt} new x, new y, query x `L+ as z, x -[ `L ]-> y, only(z, zt)"
+    run False "{x,y,z,zt} new x, new y, query x `L* as z, x -[ `L ]-> y, only(z, zt)"
 
   describe "every" $ do
-    run True  "{x, y, z} new x, query x `l+ as y, every y (x -> false)"
-    run True  "{x,y,yy,z,zt} new x, new y, new yy, x -[ `l ]-> y, y -[ `l ]-> yy, query x `l+ as z, every z (x -> true)"
-    run False "{x,y,yy,z,zt} new x, new y, new yy, x -[ `l ]-> y, y -[ `l ]-> yy, query x `l+ as z, every z (x -> false)"    
+    run True  "{x, y, z} new x, query x `L+ as y, every y (x -> false)"
+    run True  "{x,y,yy,z,zt} new x, new y, new yy, x -[ `L ]-> y, y -[ `L ]-> yy, query x `L+ as z, every z (x -> true)"
+    run False "{x,y,yy,z,zt} new x, new y, new yy, x -[ `L ]-> y, y -[ `L ]-> yy, query x `L+ as z, every z (x -> false)"    
 
   describe "min" $ do
     run False $ unlines
       [ "{x,y,z,d,ans} new x, new y, new z, new d"
-      , ", x -[ `a ]-> y, y -[ `a ]-> z, x -[ `b ]-> d, y -[ `b ]-> d"
-      , ", query x `a*`b as ans, {p} only(ans, p)"
+      , ", x -[ `A ]-> y, y -[ `A ]-> z, x -[ `B ]-> d, y -[ `B ]-> d"
+      , ", query x `A*`B as ans, {p} only(ans, p)"
       ]
       
     run True $ unlines
       [ "{x,y,z,d,ans} new x, new y, new z, new d"
-      , ", x -[ `a ]-> y, y -[ `a ]-> z, x -[ `b ]-> d, y -[ `b ]-> d"
-      , ", query x `a*`b as ans, {ps, p} min ans lexico(`b < `a) ps, only(ps, p)"
+      , ", x -[ `A ]-> y, y -[ `A ]-> z, x -[ `B ]-> d, y -[ `B ]-> d"
+      , ", query x `A*`B as ans, {ps, p} min ans lexico(`B < `A) ps, only(ps, p)"
       ]
       
     run True $ unlines
       [ "{x,y,z,d,ans} new x, new y, new z, new d"
-      , ", x -[ `a ]-> y, y -[ `a ]-> z, x -[ `b ]-> d, y -[ `b ]-> d"
-      , ", query x `a*`b as ans, {ps, p} min ans lexico(`a < `b) ps, only(ps, p)"
+      , ", x -[ `A ]-> y, y -[ `A ]-> z, x -[ `B ]-> d, y -[ `B ]-> d"
+      , ", query x `A*`B as ans, {ps, p} min ans lexico(`A < `B) ps, only(ps, p)"
       ]
       
 
   describe "filter" $ do
     run False $ unlines
-      [ "{x,y,z} new x, new y, new z, x -[ `A ]-> y, x -[ `A ]-> z"
-      , ", y -> f(), z -> g()"
+      [ "{x} new x, x -> F()" ]
+
+    run False $ unlines
+      [ "{x} new x -> F(), x -> G()" ]
+
+    run False $ unlines
+      [ "{x,y,z} new x, new y -> F(), new z -> G(), x -[ `A ]-> y, x -[ `A ]-> z"
       , ", {ans, p} query x `A as ans, only(ans, p)"
       ]
 
     run True $ unlines
-      [ "{x,y,z} new x, new y, new z, x -[ `A ]-> y, x -[ `A ]-> z"
-      , ", y -> f(), z -> g()"
-      , ", {ans, ps, p} query x `A as ans, filter ans (d where d = f()) ps, only(ps, p)"
+      [ "{x,y,z} new x, new y -> F(), new z -> G(), x -[ `A ]-> y, x -[ `A ]-> z"
+      , ", {ans, ps, p} query x `A as ans, filter ans (d where d = F()) ps, only(ps, p)"
       ]
 
   describe "lists" $ do
-    run True "(f():g()) match { (f():g():[]) -> true }"
-
-  '(x:xs)' = Cons()
+    run True "(F():G():[]) match { (F():G():[]) -> true }"
